93d8f674aefe1308fc741c0b5fe90c6c319c2bf4
ToqaMoataz <150075342+ToqaMoataz@users.noreply.github.com>
2025-08-30 00:26:05 +0300

    add movie card

---
+++ lib/App Components/movie_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_app/Core/Theme/app_colors.dart';
+
+class MovieCard extends StatelessWidget {
+  String imgURL;
+  double rating;
+  MovieCard({super.key,required this.rating,required this.imgURL});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(16.r),
+          child: Image.network(
+            imgURL,
+            height: 279.h,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Image.asset(
+                "assets/images/no image.png",
+                height: 279.h,
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+        ),
+
+        Positioned(
+            top: 12,
+            left: 12,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 4,horizontal: 6),
+              decoration: BoxDecoration(
+                color: Color(0XB5121312),
+                borderRadius: BorderRadiusGeometry.circular(10.r)
+              ),
+              child: Row(
+                children: [
+                  Text(
+                      "$rating",
+                    style: GoogleFonts.roboto(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.getPrimaryTextColor(),
+                    ),
+                  ),
+                  SizedBox(width: 5.w,),
+                  Icon(Icons.star,color: AppColors.getAccentColor(),)
+                ],
+              ),
+            )
+        )
+      ],
+    );
+  }
+}
